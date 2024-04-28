07cc31655527a952453a7d9ea8c1c8ad905bb480
DELL <sulaimabaliqis@gmail.com>
2024-04-28 18:20:04 +0100

    task

---
+++ chapterFourM/Factorial1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number float64
+	fmt.Println("Enter number: ")
+	fmt.Scanln(&number)
+	result := value(int(number))
+	fmt.Println(result)
+}
+
+func factorial(num int) float64 {
+	total := 1.0
+
+	for input := num; input > 0; input-- {
+		total *= float64(input)
+	}
+	return total
+}
+
+func value(num1 int) float64 {
+	var value float64
+	for index := 0; index < num1; index++ {
+		value += 1 / factorial(index)
+	}
+	return value
+}
